fc32de56e6226ca9822a04b8dacf1386b9b0e2c9
Sherry-404 <65493474+Sherry-404@users.noreply.github.com>
2020-09-12 22:33:35 +0800

    Add files via upload
    robot

---
+++ app.ts
@@ -0,0 +1,36 @@
+// Copyright 2019 Xlab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Application } from 'egg';
+import { GloablEvents } from './app/basic/Utils';
+
+export default class AppBootHook {
+  private app: Application;
+  constructor(app: Application) {
+    this.app = app;
+    app.messenger.setMaxListeners(20);
+  }
+
+  async willReady() {
+    this.app.messenger.broadcast(GloablEvents.READY, {});
+  }
+
+  async serverDidReady() {
+    this.app.messenger.broadcast(GloablEvents.START, {});
+  }
+
+  async beforeClose() {
+    this.app.messenger.broadcast(GloablEvents.CLOSE, {});
+  }
+}
